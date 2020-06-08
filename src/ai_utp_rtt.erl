174dc221ac3ba887a9a5cd3fd14b1c8f878fce3b
David.Gao <david.alpha.fox@gmail.com>
2020-06-08 16:47:57 +0800

    max rtt

--- src/ai_utp_rtt.erl
+++ src/ai_utp_rtt.erl
@@ -8,7 +8,7 @@
 -record(ai_utp_rtt, {
                      rtt = 500 :: integer(),
                      var = 800 :: integer(),
-                     delay = 2
+                     delay = 1
                     }).
 
 
@@ -28,11 +28,11 @@
 lost(none,_) -> #ai_utp_rtt{};
 lost(#ai_utp_rtt{delay = Delay} = RTT,LostCount)->
   Delay0 =
-    if LostCount > 0 -> Delay * 2;
-       true  -> Delay / 2
+    if LostCount > 0 -> Delay * 1.5;
+       true  -> Delay / 1.5
     end,
-  if Delay0 > 1024 -> RTT#ai_utp_rtt{delay = 1024};
-     Delay0 < 2 -> RTT#ai_utp_rtt{delay = 2};
+  if Delay0 > 20 -> RTT#ai_utp_rtt{delay = 20};
+     Delay0 < 2 -> RTT#ai_utp_rtt{delay = 1};
      true -> RTT#ai_utp_rtt{delay = Delay}
   end.
 
