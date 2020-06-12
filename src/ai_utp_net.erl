ad9e5f935f4b2c921c152ba9d775d1690bb6e72a
David Gao <david.alpha.fox@gmail.com>
2020-06-13 07:28:57 +0800

    send immediately

--- src/ai_utp_net.erl
+++ src/ai_utp_net.erl
@@ -681,7 +681,9 @@ dequeue_sndbuf(ToFill,SndBuf,Acc)->
 
 %% try to full fill one package
 do_send(#utp_net{state = ?ESTABLISHED } = Net,Proc)->
-  do_send(Net,Proc,false);
+  %% for some small and senstive data
+  %% 100ms is too big
+  do_send(Net,Proc,true);
 do_send(#utp_net{state = ?CLOSING,fin_sent = true} = Net,Proc)->
   Proc0 = ai_utp_process:error_all(Proc, {error,eshutdown}),
   {Net,Proc0};
