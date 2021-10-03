6b5a70c80c1bf9c4477b107009dbb8171f496540
David Gao <david.alpha.fox@gmail.com>
2021-10-03 18:30:20 +0800

    normal

--- include/aiutp.hrl
+++ include/aiutp.hrl
@@ -166,7 +166,7 @@
                    % per 5 seconds
                    ssthresh = ?OUTGOING_BUFFER_MAX_SIZE * ?PACKET_SIZE,
                    slow_start = true,
-                   brust = true,
+                   brust = false,
                    inbuf,
                    outbuf,
                    inque,
