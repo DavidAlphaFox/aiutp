8416e8755160a4a53dfa19f81152b364e06e5121
David.Gao <david.alpha.fox@gmail.com>
2021-10-04 14:28:48 +0800

    check time interval

--- include/aiutp.hrl
+++ include/aiutp.hrl
@@ -15,7 +15,7 @@
 -define(REORDER_BUFFER_MAX_SIZE,1024).
 -define(OUTGOING_BUFFER_MAX_SIZE,1024).
 -define(BRUST_OUTGOING_BUFFER_SIZE,1023).
--define(PACKET_SIZE,900).
+-define(PACKET_SIZE,1400).
 -define(MIN_WINDOW_SIZE,2906).
 %-define(MIN_WINDOW_SIZE,7265).
 % this is the minimum max_window value. It can never drop below this
@@ -31,7 +31,7 @@
 -define(KEEPALIVE_INTERVAL,29000). %ms
 % 29 seconds determined from measuring many home NAT devices
 -define(UTP_HEADER_SIZE,20).
--define(TIMEOUT_CHECK_INTERVAL,500).
+-define(TIMEOUT_CHECK_INTERVAL,150).
 -define(SEQ_NR_MASK,16#FFFF).
 -define(ACK_NR_MASK,16#FFFF).
 -define(TIMESTAMP_MASK, 16#FFFFFFFF).
