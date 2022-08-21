ada6b0d7296a9de5afc900799a31383b2501d8f7
David Gao <david.alpha.fox@gmail.com>
2022-08-21 16:07:43 +0800

    brust with small one

--- src/aiutp_pcb.erl
+++ src/aiutp_pcb.erl
@@ -447,7 +447,7 @@ check_timeouts_0(#aiutp_pcb{time =Now,
         check_timeouts_2(check_timeouts_1(PCB0));
        (Brust == true) and
        (CurWindowPackets > 0) and
-       ((State == ?CS_CONNECTED) or (State == ?CS_CONNECTED_FULL))->
+       (State == ?CS_CONNECTED) ->
         case check_timeouts_1(PCB0) of
           {true,_} ->
             Iter = aiutp_buffer:head(OutBuf),
